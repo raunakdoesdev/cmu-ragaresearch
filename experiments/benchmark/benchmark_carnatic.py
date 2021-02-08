eee85ce2e71541542bcdcb4582f8e4fba5dce675
Raunak Chowdhuri <raunakc@mit.edu>
2021-02-08 14:44:33 -0800

    testing is working

--- experiments/benchmark/benchmark_carnatic.py
+++ experiments/benchmark/benchmark_carnatic.py
@@ -18,7 +18,7 @@ val, test = fcd_not_train.greedy_split(test_size=0.5)
 
 train = ChromaChunkDataset(train, chunk_size=100, augmentation=transpose_chromagram, stride=10)
 data = MusicDataModule(train, val, batch_size=32)
-model = ResNet101(num_classes=max(fcd.y) + 1)
+model = ResNet152(num_classes=max(fcd.y) + 1)
 model.epochs = 50
 
 logger = WandbLogger(project='Raga Benchmark', name='Carnatic Training')
@@ -34,3 +34,6 @@ trainer = Trainer(gpus=1, logger=logger, max_epochs=model.epochs, num_sanity_val
                   val_check_interval=0.1, auto_lr_find=False, checkpoint_callback=checkpoint_callback)
 model.lr = 0.1
 trainer.fit(model, data)
+import os
+
+os.system('sudo shutdown now')
--- experiments/circular_padding/train.py
+++ experiments/circular_padding/train.py
@@ -3,6 +3,8 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 import os
+
+from experiments.circular_padding.resnet_circular_padding import ResNet34Circular
 from models.custom_resnet import *
 from src import *
 from src.data.data_module import MusicDataModule
@@ -15,22 +17,25 @@ fcd = FullChromaDataset(json_path=config['data']['metadata'],
 train, fcd_not_train = fcd.greedy_split(train_size=0.70)
 val, test = fcd_not_train.greedy_split(test_size=0.5)
 
-train = ChromaChunkDataset(train, chunk_size=100, augmentation=transpose_chromagram, stride=10)
+train = ChromaChunkDataset(train, chunk_size=100, augmentation=None, stride=10)
 data = MusicDataModule(train, val, batch_size=32)
-model = ResNet101(num_classes=max(fcd.y) + 1)
+
+mode = 'ncircular'
+
+if mode == 'circular':
+    model = ResNet34Circular(num_classes=max(fcd.y) + 1)
+else:
+    model = ResNet34(num_classes=max(fcd.y) + 1)
+
 model.epochs = 50
 
-logger = WandbLogger(project='Raga Benchmark', name='Hindustani Training')
-checkpoint_callback = ModelCheckpoint(
-    monitor='val_accuracy',
-    filepath=os.path.join(os.getcwd(), 'checkpoints/hindustani-resnet101-{epoch:02d}-{val_accuracy:.2f}'),
-    save_top_k=3,
-    mode='max',
-    verbose=True
-)
+logger = WandbLogger(project='Raga Benchmark', name='Nothing')
 
 trainer = Trainer(gpus=1, logger=logger, max_epochs=model.epochs, num_sanity_val_steps=2, deterministic=True,
-                  val_check_interval=0.1, auto_lr_find=False, checkpoint_callback=checkpoint_callback)
+                  val_check_interval=0.1, auto_lr_find=False)
 model.lr = 0.1
 trainer.fit(model, data)
-os.system('sudo shutdown now')
+
+
+
+
